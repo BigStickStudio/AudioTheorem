8d89efe932fcff6e0d175bded0146627579e7445
alephpt <alephpt1@gmail.com>
2023-03-14 16:10:25 -0500

    Added Intervals to Sequences, including Inversions

--- musictheory-rs/src/types/sequence.rs
+++ musictheory-rs/src/types/sequence.rs
@@ -2,30 +2,52 @@
 // Copyright 2023 Richard I. Christopher, NeoTec Digital. All Rights Reserved.
 //
 
-use crate::types::scale::Scale;
-use crate::types::tone::Tone;
+use crate::types::{Scale, Tone, Note, Interval};
+
+#[derive(Clone, Debug)]
+struct Chord {
+    root: Note,
+    intervals: Vec<(Note, Interval)>
+}
 
 #[derive(Clone, Debug)]
 pub struct Sequence {
     size: u8,
     tones: Vec<Tone>,
-
+    intervals: Vec<Chord>
 }
 
 impl Sequence {
     pub fn new() -> Sequence {
-        Sequence { size: 0, tones: Vec::new() }
+        Sequence { size: 0, tones: Vec::new(), intervals: Vec::new() }
+    }
+
+    fn construct_chords(&mut self) {
+        for root in self.tones.iter() {
+            let root_note = root.note();
+            let mut chord_shape = Vec::new();
+            for tone in self.tones.iter() {
+                if root != tone {
+                    chord_shape.push((tone.note(), Interval::distance(root.note(), tone.note()).unwrap()));
+                }
+            }
+            self.intervals.push(Chord{ root: root_note, intervals: chord_shape })
+        }
     }
 
     fn add_tone(&mut self, index: u8) {
         self.size += 1;
         self.tones.push(Tone::from_index(index));
         self.tones.sort_by_key(|t| t.to_index());
+        self.intervals.clear();
+        self.construct_chords()
     }
 
     fn delete_tone(&mut self, index: u8) {
         self.tones.retain(|&t| t.to_index() != index);
         self.size = self.tones.len() as u8;
+        self.intervals.clear();
+        self.construct_chords();
     }
 
     pub fn process_input(&mut self, index: u8, velocity: u8) {
