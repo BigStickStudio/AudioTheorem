4351eb683b7a3b4050566e6a3d5383c22057b89e
alephpt <alephpt1@gmail.com>
2023-03-22 22:22:21 -0500

    prepping Scene

---
+++ src/graphics/scene.rs
@@ -0,0 +1,10 @@
+//
+// Copyright 2023 Richard I. Christopher, NeoTec Digital. All Rights Reserved.
+//
+
+use super::{Texture, Instance};
+
+pub struct Scene {
+    instances: Vec<Instance>,
+    textures: Vec<Texture>
+}
